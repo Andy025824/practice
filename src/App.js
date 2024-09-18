309dcf91ef1cef2d3836a61380e3684fd57011a2
Aniket Joshi <aniketjoshi@Anikets-MacBook-Pro.local>
2024-09-18 17:39:30 +0530

    Practice Code

---
+++ src/App.js
@@ -0,0 +1,15 @@
+import { Header } from "./screens/Header";
+import { TaskList } from "./screens/TaskList";
+import './App.css';
+
+function App() {
+
+  return (
+    <div className="App">
+      <Header />
+      <TaskList />
+    </div>
+  );
+}
+
+export default App;
